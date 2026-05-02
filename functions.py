2a4fb0353e0b5ceff4338401aac43f1affbb6d29
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2026-05-02 11:14:21 -0300

    feat(functions.py): load environment variables using dotenv and add API key retrieval for OpenAI chore(requirements.txt): update requirements to include python-dotenv for environment variable management

--- functions.py
+++ functions.py
@@ -1,9 +1,13 @@
+import os
 import pandas as pd
 from config import BASE_FOLDER
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
 
 def extract_base():
-    df = pd.read_excel(BASE_FOLDER / "base_presos.xlsx")
-    
-
+    df = pd.read_excel(BASE_FOLDER / "base_presos.xlsx", sheet_name="base_resume")
     return df
     
