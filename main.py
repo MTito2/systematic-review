9d203d93232cc9064ae0f9af07b7685d6a24d5dd
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2026-05-04 19:09:08 -0300

    fix(main.py): add a space before the closing bracket in the input prompt for better readability

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 while True:
     article_number = int(input("Informe o número de artigos: "))
     construct_payload(article_number)
-    op = input("Deseja enviar payloads? [S/N]").upper()
+    op = input("Deseja enviar payloads? [S/N] ").upper()
 
     if op in("S", "SIM",):
         os.system("cls")
