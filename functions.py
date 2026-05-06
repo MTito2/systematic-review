e165dc7ec7f746f9e7e99a7f963ae8e87e971345
MTito2 <mateuscunha166@gmail.com>
2026-05-06 17:31:37 -0300

    feat(functions.py): add functions to read and export JSONL and TXT files for better data handling refactor(functions.py): modify construct_payload to create multiple payload files for batch processing fix(functions.py): update send_openai to handle multiple payload files and export batch IDs to a text file chore(functions.py): clean up code by removing unused variables and comments

--- functions.py
+++ functions.py
@@ -8,8 +8,6 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
-# payload_content = FILES_FOLDER / "content.jsonl"
-payload_content = FILES_FOLDER / "payload.jsonl"
 
 def read_json(name_path, name_file: str):
     name_json_file = name_path / name_file
@@ -18,22 +16,48 @@ def read_json(name_path, name_file: str):
         data = json.load(file)
         return data
         
+def read_jsonl(name_path, name_file: str):
+    name_jsonl_file = name_path / name_file
+    data = []
+
+    with open(name_jsonl_file, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                data.append(json.loads(line))
+    return data
+
+
 def export_json(content, name_path, name_file="content.json") -> None:
     name_json_file = name_path / name_file
 
     with open(name_json_file, "w", encoding="utf-8") as file:
         json.dump(content, file, indent=4, ensure_ascii=False)
 
+def export_txt(content, name_path, name_file) :
+    name_file_txt = name_path / name_file
+
+    with open(name_file_txt, "a", encoding="utf-8") as file:
+        file.writelines(f"{content}\n")
+
+def read_txt(name_path, name_file) :
+    name_file_txt = name_path / name_file
+
+    with open(name_file_txt, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    return content.split("\n")[:-1]
+
+
 def extract_base():
-    df = pd.read_excel(FILES_FOLDER / "base_origin.xlsx", sheet_name="base_resume")
+    df = pd.read_excel(FILES_FOLDER / "base_origin.xlsx", sheet_name="Base_Presos (2)")
     df = df[df['Elegibilidade'].isna()]
     df = df[["TI", "AB"]]
     df.to_excel(FILES_FOLDER / "base_formatted.xlsx")
     return df
     
-def construct_payload(article_number):
+def construct_payload():
     df = extract_base()
-    df = df.head(article_number)
 
     prompt_path = FILES_FOLDER / "prompt.txt"
 
@@ -62,45 +86,72 @@ def construct_payload(article_number):
 
         content.append(row_batch)
 
-    with open(payload_content, "w", encoding="utf-8") as f:
-        for row in content:
-            f.write(json.dumps(row, ensure_ascii=False) + "\n")
+    articles_division = len(content)
+    base = articles_division // 5
+    rest = articles_division % 5
+    parts = [base] * 5
+    for i in range(rest):
+        parts[i] += 1
+
+    couter_articles = 1
+    
+    result = []
+    start = 0
+
+    for size in parts:
+        end = start + size
+        result.append(content[start:end])
+        start = end
+    
+    for item in result:
+        payload_path = FILES_FOLDER / f"payload_content_{couter_articles}.jsonl"
+  
+        with open(payload_path, "w", encoding="utf-8") as f:
+            for row in item:
+                f.write(json.dumps(row, ensure_ascii=False) + "\n")
+        couter_articles += 1
 
 def send_openai():
-    my_file = client.files.create(
-        file=open(payload_content, "rb"),
-        purpose="batch"
-    )
+    for i in range(5):
+        payload_path = FILES_FOLDER / f"payload_content_{i + 1}.jsonl"
 
-    file_id = my_file.id
+        my_file = client.files.create(
+            file=open(payload_path, "rb"),
+            purpose="batch"
+        )
 
-    batch = client.batches.create(
-        input_file_id=file_id,
-        endpoint="/v1/chat/completions",
-        completion_window="24h"
-    )
+        file_id = my_file.id
 
-    return batch.id
+        batch = client.batches.create(
+            input_file_id=file_id,
+            endpoint="/v1/chat/completions",
+            completion_window="24h"
+        )
+
+        export_txt(batch.id, FILES_FOLDER, "batches.txt")
 
 def wait_processing():
-    batch_id = send_openai()
+
     status = "validating"
 
     while status not in ["failed", "completed", "cancelled"]:
         time.sleep(10)
         os.system("cls")
         
-        batch = client.batches.retrieve(batch_id)
-        status = batch.status
-        print(f"Status atual: {status}")
+        batches = read_txt(FILES_FOLDER, "batches.txt")
 
-    if status in ["processed", "completed"]:
-        return batch.output_file_id
+        for batch_id in batches:
+            os.system("cls")
+            batch = client.batches.retrieve(batch_id)
+            status = batch.status
+            print(f"Id: {batch_id}")
+            print(f"Status: {status}")
+            print()
 
-    else:
-        print("Houve um erro no processamento.")
-        print(batch.status)
-        print(batch.errors)
+            if status in ["processed", "completed"]:
+                export_txt(batch.output_file_id, FILES_FOLDER, "batch_output_id.txt")
+
+
 
 def get_only_answers():
     output_file_id = wait_processing()
@@ -123,4 +174,5 @@ def get_only_answers():
     df = pd.DataFrame(responses)
     df.to_excel(FILES_FOLDER / "response.xlsx")
 
-
+send_openai()
+wait_processing()
